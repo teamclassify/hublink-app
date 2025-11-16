26fc57ae62f75b40771990a0268b03723edd858c
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-11-16 18:36:56 -0500

    feat: create static basic user information

--- app/src/main/java/com/classify/hublink/ui/screens/Profile.kt
+++ app/src/main/java/com/classify/hublink/ui/screens/Profile.kt
@@ -1,11 +1,18 @@
 package com.classify.hublink.ui.screens
 
-import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Mail
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,7 +23,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.classify.hublink.ui.components.Logo
 import com.classify.hublink.viewmodel.AuthViewModel
 import kotlinx.coroutines.launch
 
@@ -44,18 +50,64 @@ fun ProfileScreen(authViewModel: AuthViewModel = viewModel()) {
 
                 Column(
                     modifier = Modifier
-                        .padding(vertical = 20.dp)
+                        .padding(vertical = 30.dp)
                 ) {
                     Text(
                         text = "Información Personal",
-                        fontSize = 25.sp
+                        fontSize = 25.sp,
+                        modifier = Modifier.padding(bottom = 20.dp)
                     )
-                    Text(
-                        text = "Nombre: \nAlessandro"
-                    )
-                    Text(
-                        text = "Correo electrónico: ${authViewModel.userEmail}"
-                    )
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 5.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Person,
+                            contentDescription = "User"
+                        )
+                        Spacer(modifier = Modifier.width(15.dp))
+
+                        Text(
+                            text = "Nombre:\nAlessandro"
+                        )
+                    }
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 5.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Mail,
+                            contentDescription = "Email"
+                        )
+                        Spacer(modifier = Modifier.width(15.dp))
+
+                        Text(
+                            text = "Correo electrónico: ${authViewModel.userEmail}"
+                        )
+                    }
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 5.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Phone,
+                            contentDescription = "Phone"
+                        )
+                        Spacer(modifier = Modifier.width(15.dp))
+
+                        Text(
+                            text = "Numero de telefono: \n+57 3013257023"
+                        )
+                    }
                 }
 
             Button(
