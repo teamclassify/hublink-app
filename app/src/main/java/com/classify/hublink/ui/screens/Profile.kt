cefad2ce718c3d1675d3fa85ee88b43abb1bd83c
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-11-24 16:13:17 -0500

    feat: add verticalScroll

--- app/src/main/java/com/classify/hublink/ui/screens/Profile.kt
+++ app/src/main/java/com/classify/hublink/ui/screens/Profile.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Cases
 import androidx.compose.material.icons.filled.Mail
@@ -37,7 +39,9 @@ fun ProfileScreen(authViewModel: AuthViewModel = viewModel()) {
     Surface(
         modifier = Modifier.padding(20.dp)
     ) {
-        Column {
+        Column (
+            modifier = Modifier.verticalScroll(rememberScrollState())
+        ){
 
             Box(
                 modifier = Modifier
